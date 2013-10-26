107b6dee3e44eda7360e12e8c447e817ccf02114
Moritz Beller <beller.moritz@googlemail.com>
2013-10-26 14:37:22 +0200

    character-race

--- 31.10.2013/javascript/jake_weary/app.js
+++ 31.10.2013/javascript/jake_weary/app.js
@@ -2,7 +2,7 @@
 var c = document.getElementById('content');
 
 // change inner html
-c.innerHTML = "foo";
+//c.innerHTML = "foo";
 
 var l = document.getElementById('list');
 
@@ -13,11 +13,22 @@ var changeContent = function( newText ){
 
 var changeColor = function(){
 	
-	var list = document.querySelectorAll('#list li');
+	var list = document.querySelectorAll('#list')[0].childNodes;
 	var listLength = list.length;
-	
+
+	// loop over elements
 	while(listLength > 0){
-		var element = list[listLength - 1];
+		var element = list[listLength - 1],
+		margin = 0;
+
+		// be sure that we do not have a text node
+		if(element.nodeType === 3)
+		{
+			listLength--;
+			continue;
+		}
+
+		// finally change BG color
 		if(!element.style.backgroundColor)
 		{
 			element.style.backgroundColor = "rgb(255, 123, 23)";
@@ -31,9 +42,64 @@ var changeColor = function(){
 	}
 };
 
+
+var changeMargin = function(){
+
+	var list = document.querySelectorAll('#list')[0].childNodes;
+	var listLength = list.length;
+
+	while(listLength > 0){
+		var element = list[listLength - 1],
+		margin = 0;
+
+		if(element.nodeType === 3)
+		{
+			listLength--;
+			continue;
+		}
+		
+		// push margin + 10px each click
+		// margin = parseInt( element.style.marginLeft || margin, 10);
+		// element.style.marginLeft = (margin + 10) + "px";
+		
+		// create closure to have own scope for each Element
+		// for margin and element after loop has finished 
+		(function(element){
+
+			// we need a reference for that interval to clear it later
+			var interval = setInterval(function(){
+				var margin = parseInt( element.style.marginLeft || 0, 10);
+				element.style.marginLeft = (margin + Math.random() * 10) + "px";
+
+			}, 1000/60);
+
+			// clear that interval in 1s
+			setTimeout(function(){
+				clearInterval(interval);
+				var i, el,
+					largest = largest || {
+						offsetLeft: 0
+					};
+				for(i = list.length - 1; i >= 0; i--){
+					el = list[i];
+					if(el.offsetLeft > largest.offsetLeft)
+					{
+						largest = el;
+					}
+				}
+
+				largest.style.backgroundColor = "green";
+			}, 3000);
+
+		}(element));
+		
+		listLength--;
+	}
+};
+
 var button = document.querySelector('button');
 
-button.addEventListener('click', changeColor);
+button.addEventListener('click', changeMargin);
 
 document.addEventListener('scroll', function(){
 	console.log('scrolling');

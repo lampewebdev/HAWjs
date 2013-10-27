46694f6d8e0b639aa4827b39dc37ae351e80d71f
Moritz Beller <beller.moritz@googlemail.com>
2013-10-27 21:25:20 +0100

    added keyup validation

--- 31.10.2013/javascript/form_validation/app.js
+++ 31.10.2013/javascript/form_validation/app.js
@@ -6,6 +6,10 @@ var $ = function(args){
 // or like this
 var _ = document.querySelectorAll.bind(document);
 
+////////////////////////////////////
+//  Validate on Form submit event
+////////////////////////////////////
+
 var form = $('form')[0];
 
 // on form submit event handler
@@ -18,24 +22,72 @@ form.addEventListener('submit', function(event){
 	if($username.value && $password.value && $tnbdsb.checked){
 		// yey, its valid.
 		// do not prevent the submission
-	}
-	else
-	{
-		if(!$username.value)
-		{
-			$username.className = 'error';
-		}
-		if(!$password.value)
-		{
-			$password.className = 'error';
-		}
-		if(!$tnbdsb.checked)
-		{
-
-			$tnbdsb.previousSibling.className = 'error';
-		}
-		
+	}else{
+		highlightOnError($username.value, $username);
+		highlightOnError($password.value, $password);
+		highlightOnError($tnbdsb.value, $tnbdsb.previousSibling);
 		event.preventDefault();
 	}
 });
 
+function highlightOnError(value, element, filterFn){
+	if(!value){
+		element.className = 'error';
+	}
+	else
+	{
+		if(typeof filterFn === 'function' && !filterFn(value))
+		{
+			element.className = 'error';
+		}else{
+			element.className = '';
+		}
+	}
+}
+
+///////////////////////////////////////////////
+//  Validate on keydown event @input element
+///////////////////////////////////////////////
+
+var elements = $('input[type="text"], input[type="password"]'),
+	elementsLength = elements.length;
+
+var validateString = function(string, minLength){
+	minLength = minLength || 2;
+
+	if(string.length <= minLength)
+	{
+		return false;
+	}else{
+		return true;
+	}
+};
+
+var validatePassword = function(string){
+	if(string.length < 5)
+	{
+		return false;
+	}else{
+		return true;
+	}
+};
+
+// keyup handler
+var onInputKeyup = function(event){
+	var validationFunction = null;
+
+	if( this.attributes.name.value === 'password' ){
+		validationFunction = validatePassword;
+	}else{
+		validationFunction = validateString;
+	}
+	highlightOnError(this.value, this, validationFunction);
+};
+
+// iterate over elements and assign event handler
+while(elementsLength > 0)
+{
+	var element = elements[elementsLength - 1];
+	element.addEventListener('keyup', onInputKeyup);
+	elementsLength--;
+}
